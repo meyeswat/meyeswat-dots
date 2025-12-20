e89339018691c6ffcdb4d4f5dc1836094ec46986
meyeswat <71960617+meyeswat@users.noreply.github.com>
2025-12-19 22:55:46 -0500

    Add files via upload

---
+++ .config/waybar/scripts/theme-directions.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+$terminalEmulator
+echo This config uses pywal16 to grab the colors from a selected wallpaper for system theming.
+sleep 1
+echo After configuring the wallpaper [using waypaper, accessed via CLI or sidebar shortcut], direct pywal16 to the desired image file by using "wal -i /path/to/img/file".
+echo Only one image file can be used at a time for theming.
+echo Creating a dedicated directory for wallpaper images is recommended, as well as renaming the files into something legible.
+sleep 1
+echo To use all 16 colors, add "--cols16" to the end of the command [i.e. wal -i /path/to/img/file --cols16].
+echo To create a light color sceme, add "-l".
+echo Use "wal -h" for additional help.
+
