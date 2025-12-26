3749ae9c432f1a5b132f0c9a2a3996fcdaba08fd
meyeswat <71960617+meyeswat@users.noreply.github.com>
2025-12-25 22:52:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,4 +14,5 @@ sudo cp -R /usr/share/themes/adw-gtk3/gtk-3.0 /usr/share/themes/wal
 sudo cp -R /usr/share/themes/adw-gtk3/index.theme /usr/share/themes/wal
 sudo rm -rf /usr/share/themes/wal/gtk-3.0/gtk.css
 ln -s ~/.cache/wal/colors-gtk.css /usr/share/themes/wal/gtk-3.0/
+sudo mkdir ~/.waterfox/native-messaging-hosts/
 sudo ln -s /usr/lib/mozilla/native-messaging-hosts/pywalfox.json/ ~/.waterfox/native-messaging-hosts/
