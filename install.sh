6d9c8e31a61219a962da5e91cc3c6421f8d5a7a8
meyeswat <71960617+meyeswat@users.noreply.github.com>
2026-05-31 15:23:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-sudo pacman -Syu --asexplicit alsa-lib alsa-plugins imagemagick kitty archlinux-keyring bash archlinux-appstream-data archlinux-xdg-menu pacman bluez brightnessctl systemd curl default-cursors device-mapper ffmpeg freetype2 gparted hyprgraphics hypridle hyprcursor hyprpolkitagent hyprsunset hyprutils hyprwayland-scanner imagemagick wayland java-environment-common java-runtime-common kate libxrandr libxrender linux linux-firmware make nano mkinitcpio opus print-manager prismlauncher python qemu pipewire btop hyprlock calcurse fastfetch fish hyprland kate kitty nwg-displays rofi thunar vlc waybar tumbler networkmanager hyprpaper xorg-xhost xdg-desktop-portal xdg-desktop-portal-hyprland gvfs dbus glfw sway-nc linux-headers v4l2loopback-dkms ufw pulseaudio gdm btop
+sudo pacman -Syu --asexplicit alsa-lib alsa-plugins kitty archlinux-keyring bash archlinux-appstream-data archlinux-xdg-menu pacman bluez brightnessctl systemd curl default-cursors device-mapper ffmpeg freetype2 gparted hyprgraphics hypridle hyprcursor hyprpolkitagent hyprsunset hyprutils hyprwayland-scanner imagemagick wayland java-environment-common java-runtime-common kate libxrandr libxrender linux linux-firmware make nano mkinitcpio opus print-manager prismlauncher python qemu pipewire btop hyprlock calcurse fastfetch fish hyprland kate kitty nwg-displays rofi thunar vlc waybar tumbler networkmanager hyprpaper xorg-xhost xdg-desktop-portal xdg-desktop-portal-hyprland gvfs dbus glfw sway-nc linux-headers v4l2loopback-dkms ufw lightdm-slick-greeter btop
 
 xdg-mime default thunar.desktop inode/directory
-sudo systemctl enable --now gdm
 
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
