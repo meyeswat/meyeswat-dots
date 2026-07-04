09bcab20f7a88d25aa387963ee40e0e675ff8f00
meyeswat <71960617+meyeswat@users.noreply.github.com>
2026-07-04 13:18:09 -0400

    Add files via upload

--- .config/hypr/keybinds.lua
+++ .config/hypr/keybinds.lua
@@ -72,3 +72,6 @@ hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_S
 hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+
+hl.bind("XF86Calculator", hl.dsp.exec_cmd("gnome-calculator"))
+
